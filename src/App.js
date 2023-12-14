df1fe63d51c11d608b4d987cc6d5516f9ffad376
성백준 <toypiano@naver.com>
2023-12-14 17:03:02 +0900

    MyComponent

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
-import './App.css';
+import MyComponent from './MyComponent';
 
-import React, { Component } from 'react';
-
-class App extends Component {
-  render() {
-    const name = 'react';
-    return <div className="react">{name}</div>;
-  }
-}
+const App = () => {
+  return <MyComponent />;
+};
 
 export default App;
